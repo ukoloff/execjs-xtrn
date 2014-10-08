bcc22f154a48204303658866d7727b8390c03221
Stas Ukolov <ukoloff@gmail.com>
2014-10-08 07:56:46 +0600

    Move tests to a file

--- test/child.rb
+++ test/child.rb
@@ -1,69 +1,12 @@
-# encoding: utf-8
-class TestChild < Minitest::Test
+require_relative 'shagi'
 
-  M=ExecJS::Xtrn
+class TestChild < Shagi
 
   Spawn=2
   Children=[M::Node, M::Wsh]
 
   def say(code)
     @child.say code
-  end
-
-  def assert_ok(result, code)
-    r=say code
-    refute r.key? 'err'
-    assert_equal r, {'ok'=>result}
-  end
-
-  def assert_err(code)
-    assert say(code)['err']
-  end
-
-  def shag_math
-    assert_ok 42, 'return 6*7'
-    assert_ok 3,  'return Math.round(Math.PI)'
-  end
-
-  Chars='Япония, 中华, Russia'
-  Codes=[1071, 1087, 1086, 1085, 1080, 1103, 44, 32, 20013, 21326, 44, 32, 82, 117, 115, 115, 105, 97]
-
-  def shag_intl
-    assert_ok Codes, <<-EOJ
-      var s='#{Chars}'
-      var r=[]
-      for(var i=0; i<s.length; i++) r.push(s.charCodeAt(i))
-      return r
-    EOJ
-
-    assert_ok Chars, <<-EOJ
-      var c=#{Codes}
-      var s=''
-      for(var i=0; i<c.length; i++) s+=String.fromCharCode(c[i])
-      return s
-    EOJ
-  end
-
-  def shag_error
-    assert_err '#'      # Syntax
-    assert_err 'none'   # Runtime
-    assert_err false    # Argument
-    assert_err key: 2   # the same
-  end
-
-  def shag_null
-    assert_equal say(''), {}
-    assert_ok nil, 'return null'
-  end
-
-  def shag_vars
-    assert_err 'localVar'
-    say 'var localVar=1'
-    assert_err 'localVar'
-
-    assert_err 'globalVar'
-    say "globalVar=#{v=rand 1000}"
-    assert_ok v, 'return globalVar'
   end
 
   def children
@@ -73,7 +16,7 @@ class TestChild < Minitest::Test
   end
 
   def self.build
-    instance_methods(false).grep(/^shag_/).each do |m|
+    instance_methods.grep(/^shag_/).each do |m|
       Children.each_with_index  do |klass, idx|
         (1..Spawn).each do |n|
           define_method("test_#{m.to_s.sub /.*?_/, ''}_#{klass.name.split(/\W+/).last}_#{n}")do
---
+++ test/shagi.rb
@@ -0,0 +1,66 @@
+# encoding: utf-8
+class Shagi < Minitest::Test
+
+  M=ExecJS::Xtrn
+
+  def say(code)
+    raise NotImplementedError, self.class.name
+  end
+
+  def assert_ok(result, code)
+    r=say code
+    refute r.key? 'err'
+    assert_equal r, {'ok'=>result}
+  end
+
+  def assert_err(code)
+    assert say(code)['err']
+  end
+
+  def shag_math
+    assert_ok 42, 'return 6*7'
+    assert_ok 3,  'return Math.round(Math.PI)'
+  end
+
+  Chars='Япония, 中华, Russia'
+  Codes=[1071, 1087, 1086, 1085, 1080, 1103, 44, 32, 20013, 21326, 44, 32, 82, 117, 115, 115, 105, 97]
+
+  def shag_intl
+    assert_ok Codes, <<-EOJ
+      var s='#{Chars}'
+      var r=[]
+      for(var i=0; i<s.length; i++) r.push(s.charCodeAt(i))
+      return r
+    EOJ
+
+    assert_ok Chars, <<-EOJ
+      var c=#{Codes}
+      var s=''
+      for(var i=0; i<c.length; i++) s+=String.fromCharCode(c[i])
+      return s
+    EOJ
+  end
+
+  def shag_error
+    assert_err '#'      # Syntax
+    assert_err 'none'   # Runtime
+    assert_err false    # Argument
+    assert_err key: 2   # the same
+  end
+
+  def shag_null
+    assert_equal say(''), {}
+    assert_ok nil, 'return null'
+  end
+
+  def shag_vars
+    assert_err 'localVar'
+    say 'var localVar=1'
+    assert_err 'localVar'
+
+    assert_err 'globalVar'
+    say "globalVar=#{v=rand 1000}"
+    assert_ok v, 'return globalVar'
+  end
+
+end
