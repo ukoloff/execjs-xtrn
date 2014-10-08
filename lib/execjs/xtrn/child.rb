d95e72543b6636802d642b70214d05f22c43c759
Stas Ukolov <ukoloff@gmail.com>
2014-10-08 18:03:39 +0600

    Gather statistics

--- lib/execjs/xtrn/child.rb
+++ lib/execjs/xtrn/child.rb
@@ -2,6 +2,8 @@ require 'json'
 
 class ExecJS::Xtrn::Child
 
+  @@stats={c: 0} # children
+
   def initialize(options)
     i=IO.pipe
     o=IO.pipe
@@ -18,11 +20,33 @@ class ExecJS::Xtrn::Child
       @stdin.set_encoding options[:encoding]
       @stdout.set_encoding options[:encoding]
     end
+    @@stats[:c]+=1
+    @stats={}
   end
 
   def say(obj)
-    @stdin.puts JSON.generate([obj])[1...-1]
-    JSON.load @stdout.gets
+    delta={
+      n: 1, # say calls
+      o: 0, # out bytes
+      i: 0, # in bytes
+      t: Time.now # time spent
+    }
+    @stdin.puts delta[:o]=JSON.generate([obj])[1...-1]
+    JSON.load(delta[:i]=@stdout.gets)
+      .tap do
+        delta[:t]=Time.now-delta[:t]
+        delta[:i]=delta[:i].length
+        delta[:o]=delta[:o].length
+        [@stats, @@stats].each{|var| delta.each{|k, v| var[k]||=0; var[k]+=v}}
+      end
+  end
+
+  def self.stats
+    @@stats.dup
+  end
+
+  def stats
+    @stats.dup
   end
 
 end
