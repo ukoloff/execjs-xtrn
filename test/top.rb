b4e7ef0c44a4dcadbcca14b1ac2b7ecf490d74a2
Stas Ukolov <ukoloff@gmail.com>
2014-10-11 21:05:54 +0600

    Test ExecJS.stats

--- test/top.rb
+++ test/top.rb
@@ -18,4 +18,14 @@ class TestTop < Minitest::Test
     assert_equal 8, ctx.call('dec', 9)
   end
 
+  def test_stats
+    ExecJS.exec '//'
+    s=ExecJS.stats
+    %w(c i o t).each{|sym| assert s[sym.to_sym]}
+    ExecJS.exec ' '
+    assert_equal s[:n], ExecJS.stats[:n]
+    ExecJS.exec 'null'
+    assert_operator s[:n], :<, ExecJS.stats[:n]
+  end
+
 end
