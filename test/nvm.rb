c5e9f02cf468d38b3905dd8387a8a71bc677f772
Stas Ukolov <ukoloff@gmail.com>
2016-04-28 07:19:36 +0500

    Remove scaffolding

--- test/nvm.rb
+++ test/nvm.rb
@@ -5,10 +5,7 @@ class TestNvm < Shagi
   Child=M::Nvm
 
   def say(code)
-    # puts "s: child=#{@child} vm=#{@vm}, code=#{code}"
-    r=@child.say vm: @vm, js: code
-    # puts "r: #{r}"
-    r
+    @child.say vm: @vm, js: code
   end
 
   def buildVMs
@@ -21,7 +18,7 @@ class TestNvm < Shagi
   def self.build
     ancestors[1].instance_methods(false).grep(/^shag_/).each do |m|
       (1..Spawn).each do |n|
-        define_method("test_#{m.to_s.sub /.*?_/, ''}_#{n}")do
+        define_method("test_#{m.to_s.sub(/.*?_/, '')}_#{n}")do
           skip unless Child::Valid
           @vm=(@@vms||=buildVMs)[n-1]
           @child=@@child
