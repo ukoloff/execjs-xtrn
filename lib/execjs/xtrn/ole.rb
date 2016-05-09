7639ca23c76c0c96e5c47da5552aabd73ae3787e
Stas Ukolov <ukoloff@gmail.com>
2016-05-09 12:34:16 +0500

    Use ScriptControl

--- lib/execjs/xtrn/ole.rb
+++ lib/execjs/xtrn/ole.rb
@@ -1,6 +1,7 @@
 require_relative 'wsh'
 
 class ExecJS::Xtrn::Ole < ExecJS::Xtrn::Wsh
+
   def self.valid?
     return unless Gem.win_platform?
     require 'win32ole'
@@ -13,6 +14,16 @@ class ExecJS::Xtrn::Ole < ExecJS::Xtrn::Wsh
 
   def exec code
     return if (code=code.to_s.strip).length==0
-    # TODO: Implement OLE
+    vm.eval "new Function(#{JSON.dump code})()"
   end
+
+  private
+
+  def vm
+    return @vm if @vm
+    @vm = WIN32OLE.new 'ScriptControl'
+    @vm.Language = 'JScript'
+    @vm
+  end
+
 end
