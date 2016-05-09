a7938ee9e351d0fd45a4e92a68a970253ac0c014
Stas Ukolov <ukoloff@gmail.com>
2016-05-09 13:18:14 +0500

    Store stats

--- lib/execjs/xtrn/ole.rb
+++ lib/execjs/xtrn/ole.rb
@@ -14,13 +14,30 @@ class ExecJS::Xtrn::Ole < ExecJS::Xtrn::Wsh
 
   def exec code
     return if (code=code.to_s.strip).length==0
-    vm.eval "new Function(#{JSON.dump code})()"
+    result = nil
+    delta={
+      n: 1, # say calls
+      i: 0, # in bytes
+      o: 0, # out bytes
+      t: Time.now # time spent
+    }
+    result = vm.eval "new Function(#{JSON.dump code})()"
+  ensure
+    delta[:t]=Time.now-delta[:t]
+    delta[:i]=code.length
+    delta[:o]=JSON.dump(result).length if result
+    @statz.each{|var| delta.each{|k, v| var[k]||=0; var[k]+=v}}
   end
 
   private
 
+  @stats = @@stats = {c: 0}
+
   def vm
     return @vm if @vm
+    @stats||={}
+    @@stats[:c]+=1
+    @statz=[@stats, @@stats]
     @vm = WIN32OLE.new 'ScriptControl'
     @vm.Language = 'JScript'
     @vm
