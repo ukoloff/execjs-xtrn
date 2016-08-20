643a189665b96180805f347783b8458de8516a63
Stas Ukolov <ukoloff@gmail.com>
2016-08-20 16:35:54 +0500

    Test both VM interfaces

--- test/vm.rb
+++ test/vm.rb
@@ -2,28 +2,27 @@ require_relative 'shagi'
 
 class TestVm < Shagi
 
-  Child=M::Nvm
+  Children=[M::Nvm, M::Wvm]
 
   def say(code)
     @child.say vm: @vm, js: code
   end
 
-  def buildVMs
-    @@child=child=M::Child.new(Child::Run)
-    (1..Spawn).map do
-      child.say(vm: 0)['vm']
-    end
-  end
-
   def self.build
-    ancestors[1].instance_methods(false).grep(/^shag_/).each do |m|
+    Children.each do |ch|
+      valid = ch::Valid
+      prefix = "test_#{ch.name.split(/\W+/).last}_"
+      child = M::Child.new ch::Run if valid
       (1..Spawn).each do |n|
-        define_method("test_#{m.to_s.sub(/.*?_/, '')}_#{n}")do
-          skip unless Child::Valid
-          @vm=(@@vms||=buildVMs)[n-1]
-          @child=@@child
-          send m
-        end
+        vm = child.say(vm: 0)['vm'] if valid
+          ancestors[1].instance_methods(false).grep(/^shag_/).each do |m|
+            define_method("#{prefix}#{m.to_s.sub(/.*?_/, '')}_#{n}")do
+              skip unless valid
+              @child = child
+              @vm = vm
+              send m
+            end
+          end
       end
     end
   end
