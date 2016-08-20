7869375f5978238663f7266cf99f376f5bb49f35
Stas Ukolov <ukoloff@gmail.com>
2016-08-20 16:15:21 +0500

    Patch for Ruby x64

--- lib/execjs/xtrn/wvm.rb
+++ lib/execjs/xtrn/wvm.rb
@@ -5,4 +5,15 @@ require_relative 'wsh'
 
 class ExecJS::Xtrn::Wvm < ExecJS::Xtrn::Wsh
   include ExecJS::Xtrn::VM
+
+  # Force 32-bit cscript
+  def self.patch64
+    return unless Gem.win_platform?
+    return unless File.exist? exe =
+      File.join(ENV['windir'], 'syswow64/cscript.exe')
+    args = Run[:args].dup
+    args[0] = exe
+    @run32 = Run.merge args: args
+  end
+  Run = @run32 if self.patch64
 end
